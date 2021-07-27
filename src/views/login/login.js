2d0e6f58cfe880a9f2dddc7588d0ce38e609fe26
Isaac Yriarte <isaacyagi@gmail.com>
2021-07-27 15:25:01 -0500

    Feat: Implementa un FormInput para correo en login

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,8 +1,28 @@
-import { React } from 'react';
+import { React, useState } from 'react';
+import FormInput from '../../utils/components/FormInput/form-input';
 
 export default function Login(){
     
+    const [inputValue, setInputValue] = useState("");
+
+    const inputConfig = {
+        label:{
+            text: "Email"
+        },
+        input:{
+            type: "text",
+            placeholder: "Please insert your email...",
+            value:  inputValue,
+            onChange: (event) => setInputValue(event.target.value)
+
+        }
+    }
     return (
-        <div>Hello from login </div>
+        <>
+            <div>Hello from login </div>
+            <FormInput
+                data={inputConfig}
+            ></FormInput>
+        </>
     );
 }
