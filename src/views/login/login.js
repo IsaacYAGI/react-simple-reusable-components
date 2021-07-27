81a597927ec2c13232d4bb718be16166969fa0d4
Isaac Yriarte <isaacyagi@gmail.com>
2021-07-27 15:30:47 -0500

    Feat: Agrega campo password

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -4,24 +4,39 @@ import FormInput from '../../utils/components/FormInput/form-input';
 export default function Login(){
     
     const [inputValue, setInputValue] = useState("");
+    const [password, setPassword] = useState("");
 
-    const inputConfig = {
+    const inputEmailConfig = {
         label:{
             text: "Email"
         },
         input:{
             type: "text",
             placeholder: "Please insert your email...",
-            value:  inputValue,
+            value: inputValue,
             onChange: (event) => setInputValue(event.target.value)
+        }
+    }
 
+    const inputPasswordConfig = {
+        label:{
+            text: "Password"
+        },
+        input:{
+            type: "password",
+            placeholder: "Please insert your password...",
+            value: password,
+            onChange: (event) => setPassword(event.target.value)
         }
     }
     return (
         <>
             <div>Hello from login </div>
             <FormInput
-                data={inputConfig}
+                data={inputEmailConfig}
+            ></FormInput>
+            <FormInput
+                data={inputPasswordConfig}
             ></FormInput>
         </>
     );
