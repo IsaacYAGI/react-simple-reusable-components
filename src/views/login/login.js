5f04d65abad53e2611e05ccc1aa1e4cce6aa948e
Isaac Yriarte <isaacyagi@gmail.com>
2021-07-27 17:08:22 -0500

    Feat: Implementa boton loading al formulario

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,10 +1,12 @@
 import { React, useState } from 'react';
 import FormInput from '@utils/components/FormInput/form-input';
+import Button from '@utils/components/Button/button';
 
 export default function Login(){
     
     const [inputValue, setInputValue] = useState("");
     const [password, setPassword] = useState("");
+    const [buttonLoading, setButtonLoading] = useState(false)
 
     const inputEmailConfig = {
         label:{
@@ -29,15 +31,47 @@ export default function Login(){
             onChange: (event) => setPassword(event.target.value)
         }
     }
+
+    const fetchApi = () => {
+        setButtonLoading(true);
+        setTimeout( 
+            () => setButtonLoading(false),
+            3000
+        )
+    }
+
+    const buttonSubmitConfig = {
+        buttonTextValue: "Submit", 
+        typeValue: "button",
+        buttonOnClickFunc: fetchApi, 
+        showLoading: buttonLoading, 
+        disabledValue: buttonLoading
+    }
     return (
         <>
             <div>Hello from login </div>
-            <FormInput
-                data={inputEmailConfig}
-            ></FormInput>
-            <FormInput
-                data={inputPasswordConfig}
-            ></FormInput>
+                
+            <form>
+                <div>
+
+                <FormInput
+                    data={inputEmailConfig}
+                ></FormInput>
+                </div>
+                <div>
+
+                <FormInput
+                    data={inputPasswordConfig}
+                ></FormInput>
+                </div>
+                <div>
+                    <Button
+                        data={buttonSubmitConfig}
+                    >
+
+                    </Button>
+                </div>
+            </form>
         </>
     );
 }
